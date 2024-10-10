9b69d60c4dec17797206720e69138292992cb810
Ariel Roy <ariel@hyfi.io>
2024-10-10 13:24:25 -0400

    update links

--- netlify/functions/submitForm.js
+++ netlify/functions/submitForm.js
@@ -1,27 +1,27 @@
 const fetch = require('node-fetch');
 
-exports.handler = async (event) => {
-  if (event.httpMethod !== 'POST') {
+exports.handler = async (event, context) => {
+  if (event.httpMethod === 'POST') {
+    const data = JSON.parse(event.body);
+
+    // Forward data to Google Apps Script
+    const response = await fetch('https://script.google.com/macros/s/AKfycbwMs_rvEmoIZ82PDzZB2GQqINUiuTHi9GtzLfDuDMeEC1sWHYmOQRQ00Nsf6qkPtLbr/exec', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    });
+
+    const result = await response.text();
+    return {
+      statusCode: 200,
+      body: result,
+    };
+  } else {
     return {
       statusCode: 405,
-      body: 'Method Not Allowed',
+      body: "Method Not Allowed",
     };
   }
-
-  const formData = JSON.parse(event.body);
-  
-  // Send the data to Google Apps Script
-  const response = await fetch('https://script.google.com/macros/s/AKfycbwMs_rvEmoIZ82PDzZB2GQqINUiuTHi9GtzLfDuDMeEC1sWHYmOQRQ00Nsf6qkPtLbr/exec', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(formData),
-  });
-
-  const result = await response.text();
-  return {
-    statusCode: 200,
-    body: result,
-  };
 };
